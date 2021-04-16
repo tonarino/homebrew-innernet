9bd3a07bfdeddcb8f2f86ee21f7d0af8468f6e30
Jake McGinty <me@jake.su>
2021-04-16 11:53:19 +0900

    add innernet back

---
+++ Formula/innernet.rb
@@ -0,0 +1,29 @@
+class Innernet < Formula
+  desc "A private network system that uses WireGuard under the hood."
+  homepage "https://github.com/tonarino/innernet"
+  url "https://github.com/tonarino/innernet/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "308f5b7de6dcb017fd76fd0df3ceac723dfc6ca709d6f3ebe35c0c15457f968c"
+  license "MIT"
+  head "https://github.com/tonarino/innernet.git", :branch => "main"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "rust" => :build
+  depends_on "wireguard-go"
+
+  def install
+    cd "client" do
+      system "cargo", "install", *std_cargo_args
+      #man8.mkpath
+      #man8.install "doc/innernet.8"
+    end
+    bin.install_symlink "innernet" => "inn"
+  end
+
+  test do
+    system "false"
+  end
+end
