2abac43a17b21ab94ca0e532550cc24fb82c830f
Jake McGinty <me@jake.su>
2021-09-19 18:21:53 +0900

    v1.5.0

--- Formula/innernet.rb
+++ Formula/innernet.rb
@@ -1,8 +1,8 @@
 class Innernet < Formula
   desc "Creates private networks that use WireGuard under the hood"
   homepage "https://github.com/tonarino/innernet"
-  url "https://github.com/tonarino/innernet/archive/refs/tags/v1.4.1.tar.gz"
-  sha256 "b977940978a7701b9cac2f3674d8e941c2dad65bb03bc23139be433757b14474"
+  url "https://github.com/tonarino/innernet/archive/refs/tags/v1.5.0.tar.gz"
+  sha256 "b5e283e1953f9183e2c77814fa6b39f08f4914509ff17f91f94599824693bed2"
   license "MIT"
   head "https://github.com/tonarino/innernet.git", { branch: "main" }
 
