d81d78e312ab18abc703cd07e496e6f97898f07a
Jake McGinty <me@jake.su>
2022-03-21 21:59:20 +0900

    v1.5.4

--- Formula/innernet.rb
+++ Formula/innernet.rb
@@ -1,8 +1,8 @@
 class Innernet < Formula
   desc "Creates private networks that use WireGuard under the hood"
   homepage "https://github.com/tonarino/innernet"
-  url "https://github.com/tonarino/innernet/archive/refs/tags/v1.5.2.tar.gz"
-  sha256 "e5ebec0b17a74d3bb241faeeef7ea2069aa61db169e3de07394b1f62d4b1b830"
+  url "https://github.com/tonarino/innernet/archive/refs/tags/v1.5.4.tar.gz"
+  sha256 "235fab9fbf67be8667ac0ec89f7fc45771b28d85be791ac3d9ae9a4d21e15673"
   license "MIT"
   head "https://github.com/tonarino/innernet.git", { branch: "main" }
 
@@ -19,39 +19,8 @@ class Innernet < Formula
     cd "client" do
       system "cargo", "install", *std_cargo_args
     end
-    bin.install_symlink "innernet" => "inn"
   end
 
-  def plist
-    <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-        <dict>
-          <key>Label</key>
-          <string>#{plist_name}</string>
-          <key>KeepAlive</key>
-          <true/>
-          <key>RunAtLoad</key>
-          <true/>
-          <key>ProgramArguments</key>
-          <array>
-            <string>#{opt_bin}/innernet</string>
-            <string>up</string>
-            <string>--daemon</string>
-            <string>--interval</string>
-            <string>60</string>
-          </array>
-          <key>UserName</key>
-          <string>root</string>
-          <key>StandardOutPath</key>
-          <string>/dev/null</string>
-          <key>StandardErrorPath</key>
-          <string>/dev/null</string>
-        </dict>
-      </plist>
-    EOS
-  end
   test do
     cd "client" do
       system "cargo", "test", *std_cargo_args
